45500d2821494311abf13eb5a88a4575a64c3855
TranChinh03 <21520166@gm.uit.edu.vn>
2024-06-27 17:08:00 +0700

    Init dictionary

---
+++ web/src/component/NavBar/nav_Bar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./nav_bar.module.css";
+import { NavLink, useNavigate } from "react-router-dom";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.mainContainer}>
+      <NavLink to="/" className={styles.txtWeb}>
+        Henzesture
+      </NavLink>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? styles.activeTxt : styles.txtNav
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          isActive ? styles.activeTxt : styles.txtNav
+        }
+      >
+        About us
+      </NavLink>
+      <NavLink
+        to="/model"
+        className={({ isActive }) =>
+          isActive ? styles.activeTxt : styles.txtNav
+        }
+      >
+        Model
+      </NavLink>
+      <NavLink
+        to="/game"
+        className={({ isActive }) =>
+          isActive ? styles.activeTxt : styles.txtNav
+        }
+      >
+        Game
+      </NavLink>
+      <NavLink
+        to="/dictionary"
+        className={({ isActive }) =>
+          isActive ? styles.activeTxt : styles.txtNav
+        }
+      >
+        Dictionary
+      </NavLink>
+    </div>
+  );
+};
